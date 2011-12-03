1c7943eaa916d5a7cc7bdd27388a20bb9069fb27
Justin Keville <justin.keville@gmail.com>
2011-12-03 17:42:33 -0500

    adding everything

---
+++ pong.py
@@ -0,0 +1,62 @@
+import sys, pygame, random, math
+pygame.init()
+
+size = width, height = 800, 600
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("ball.jpg")
+ballrect = ball.get_rect()
+ballrect = ballrect.move(50,50)
+
+paddle1 = pygame.image.load("paddle.jpg")
+pdl1rect = paddle1.get_rect()
+
+
+paddle2 = pygame.image.load("paddle.jpg")
+pdl2rect = paddle2.get_rect()
+pdl2rect = pdl2rect.move(width - pdl2rect.width,0)
+
+
+def EnemyScore(ballrect):
+	print "He scored."
+	ballrect = ballrect.move(width/2, 0)
+
+def YouScore(ballrect):
+	print "You scored."
+	ballrect = ballrect.move(width/2 * -1, 0)
+
+
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+
+	ballrect = ballrect.move(speed)
+
+	# Someone got a point
+	if ballrect.left < 0:
+		EnemyScore(ballrect)
+	if ballrect.right > width:
+		YouScore(ballrect)
+
+	# Ball colliding with top or bottom
+	if ballrect.top < 0 or ballrect.bottom > height:
+		speed[1] = -speed[1]
+
+	# Ball colliding with a paddle
+	if pdl1rect.colliderect(ballrect) or pdl2rect.colliderect(ballrect):
+		speed[0] = -speed[0]
+
+	#Enemy paddle movement
+	difference = pdl2rect.top - pdl2rect.height/2 -  ballrect.top
+	if difference != 0:
+		pdl2rect.move(0, difference / math.fabs(difference))
+
+	screen.fill(black)
+	screen.blit(ball, ballrect)
+	screen.blit(paddle1, pdl1rect)
+	screen.blit(paddle2, pdl2rect)
+	pygame.display.flip()
+
